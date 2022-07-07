5df9aef40481749aa56c437dbad4733756883a70
JosephPoole <Joseph.poole90@gmail.com>
2022-07-07 10:48:18 -0500

    first commit

---
+++ Selectionstatementsexercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selectionstatementsexercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Guess a number between 1 and 1000.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+
+                Console.WriteLine(" The Number you entered is too low.");
+
+            else if (userInput > favNumber)
+
+                Console.WriteLine(" The Number you entered is too High.");
+
+            else
+
+                Console.WriteLine(" Congratulatins you guessed right!");
+        
+        }
+    }
+}
---
+++ Selectionstatementsexercise2/Selectionexercise2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selectionexercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your favorite school subject from the following. \n English, Math, Science, History, or Art");
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "english":
+                    Console.WriteLine(" Your favorite subject is english!");
+                    break;
+                case "math":
+                    Console.WriteLine("Your favorite subject is math!");
+                    break;
+                case "science":
+                    Console.WriteLine(" Your favorite subject is science!!");
+                    break;
+                case "history":
+                    Console.WriteLine(" Your Favorite subject is history?");
+                    break;
+                case "art":
+                    Console.WriteLine(" Your favorite subject is art?! Mine was too!!");
+                    break;}
+
+        }
+    }
+}
